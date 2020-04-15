88f9963b66064b55392a733efd3310ac76012d05
JustSierra <63554135+JustSierra@users.noreply.github.com>
2020-04-15 11:55:43 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,94 +1,32 @@
 ﻿using System;
 
-namespace Deliverable1
+namespace Deliverable2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            String vacationType;
-            char groupSize;
-            string result;
 
-            Console.WriteLine("What kind of vaction would you like to go on? Select one of the following");
-            Console.WriteLine("(A) Musical ");
-            Console.WriteLine("(B) Tropical ");
-            Console.WriteLine("(C) Adventurous");
-
-            Console.WriteLine("What is your selection?");
-           
-           
-            Console.ReadLine();
-
-            Console.WriteLine("How many people are going?");
-            Console.WriteLine("(D) 1-2" );
-            Console.WriteLine("(E) 3-5");
-            Console.WriteLine("(F) 6+");
-
-            char selection = Console.ReadKey().KeyChar;
-
-            Console.ReadLine();
-           
-
-          
-
-            switch (selection)
+            string s;
+            Console.WriteLine(" Enter your sentence: ");
+            s = Console.ReadLine();
+            foreach (char c in s)
             {
-                case 'A':
-                case 'D':
-                    Console.WriteLine("Since you chose a musical trip with 1-2 people, you should fly first class to New Orleans.");
-                    break;
-                case 'a':
-                case 'E':
-                    Console.WriteLine("Since you chose a musical trip with 3-5 people, you should fly first class to New Orleans.");
-                    break;
-                           
-                default:
-                    Console.WriteLine("Since you chose a musical trip with probably 6+ people, you should fly first class to New Orleans.");
-                    break;                
-            }
-
-            Console.ReadLine();
-
-            switch (selection)
-            {
-                case 'B':
-                case 'D':
-                    Console.WriteLine("Since you chose a tropical trip with 1-2 people, you should get a charter flight for a beach vaction in Mexico");
-                     break;
-                case 'b':
-                case 'E':
-                    Console.WriteLine("Since you chose a tropical trip with 3-5 people, you should get a charter flight for a beach vaction in Mexico");
-                    break;
-                default:
-                    Console.WriteLine("Since you chose a tropical trip with probably 6+ people, you should get a charter flight for a beach vaction in Mexico");
-                    break;
-                    
-            }
-
-            Console.ReadLine();
-
-            switch (selection)
-            {
-                case'C':
-                case'D':
-                    Console.WriteLine("Since you chose an adventurous trip with 1-2 people, you should take a helicopter for whitewater rafting in the grand canyon");
-                    break;
-                case 'c':
-                case 'E':
-                    Console.WriteLine("Since you chose an adventurous trip with 3-5 people, you should take a helicopter for whitewater rafting in the grand canyon");
-                    break;
-              
-                default:
-                    Console.WriteLine("Since you chose an adventurous trip with probably 6+ people, you should take a helicopter for whitewater rafting in the grand canyon");
-                    break;
-
-                    Console.ReadLine();
+                Console.WriteLine((int)c);
             }
 
 
-
+            Console.ReadLine();
 
         }
+          
+                
+
+     
+
+        
+
+        
+
     }
 }
